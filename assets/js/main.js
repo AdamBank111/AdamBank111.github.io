051c7ea872fa2ab1a016bb173bcea3db89247e80
AdamBank111 <109998041+AdamBank111@users.noreply.github.com>
2022-07-26 16:11:36 -0400

    Added local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
-let count = 0;
+let count;
 const btn = document.getElementById("btn");
 
+console.log(localStorage.getItem("sCount"))
+
+//Check if we have already saved a count
+if (localStorage.getItem("sCount") == null) {
+    localStorage.setItem("sCount", 0);
+    count = 0
+} else {
+    count = parseInt(localStorage.getItem("sCount"));
+    btn.innerText = count;
+}
+
+
+//When the button is clicked increase the count and update
 btn.addEventListener("click", ()=>{
     count = count + 1;
+    localStorage.setItem("sCount", count);
     btn.innerText = count;
 })
-
-function countUp(count) {
-    count = count + 1
-    document.getElementById("but").innerHTML = count;
-}
